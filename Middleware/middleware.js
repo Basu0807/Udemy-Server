67c2bd131f23f8537b2c5f764d1f0507f6bbae26
Basu0807 <vasupandeyji.98@gmail.com>
2023-12-09 01:12:04 +0530

    commit

---
+++ Middleware/middleware.js
@@ -0,0 +1,27 @@
+
+
+const jwt =require('jsonwebtoken')
+const secretkey ="vasu"
+
+const middleware=(req,res,next)=>{
+const BearerToken = req.headers["authorization"]
+// console.log(BearerToken);
+
+
+if(BearerToken){
+    const token = BearerToken.split(" ")[1]
+    
+    const validate = jwt.verify(token, secretkey);
+    if(validate){
+        next();
+    }
+
+}
+else{
+    return res.send("Please Login to add Items")
+
+}
+
+}
+
+module.exports=middleware;
--- Routes/UserRoutes.js
+++ Routes/UserRoutes.js
@@ -1,7 +1,9 @@
 const UserRoutes = require('express').Router()
-const {registerController, loginController} = require('../Controller/loginSignup')
+const {registerController, loginController, ProfileController} = require('../Controller/loginSignup')
+const middleware = require('../Middleware/middleware')
 
 UserRoutes.post('/register',registerController)
 UserRoutes.post('/login',loginController)
+UserRoutes.post('/profile',middleware,ProfileController)
 
 module.exports =UserRoutes
