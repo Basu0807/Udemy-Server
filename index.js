28ae7b4ce14e34f785e01b7ad245c6578a0b9b19
Basu0807 <vasupandeyji.98@gmail.com>
2023-12-09 01:57:45 +0530

    commit

--- index.js
+++ index.js
@@ -19,6 +19,29 @@ app.get('/',(req,res)=>{
     res.send("This is our HomePage")
     console.log("Home page");
     })
+
+    app.post("/checkout",async(req,res)=>{
+        const {products}=req.body
+        console.log(products)
+        const lineItems =products.map((product)=>({
+            price_data:{
+                currency:"inr",
+                product_data:{
+                    name:product.heading,
+                },
+                unit_amount:product.price * 100
+            },
+            quantity:product.quantity
+           }))
+           const session =await stripe.checkout.sessions.create({
+            payment_method_types:["card"],
+             line_items:lineItems,
+            mode:"payment",
+            success_url:"https://dainty-pothos-8d469f.netlify.app/",
+            cancel_url:"https://dainty-pothos-8d469f.netlify.app/",
+        })
+        res.json({id:session.id})
+        })
     
     app.listen(Port, async()=>{
         try {
